ea29476a6c4774532f4ec4678b39e72c53b4ad58
RyannKim327 <rksesgundo123@gmail.com>
2023-04-01 02:08:55 +0800

    04-01-23 02:08 [In the beginning]

---
+++ res/script.js
@@ -0,0 +1,17 @@
+class ${
+	constructor(__name__){
+		if(__name__.startsWith("#")){
+			this.name = document.getElementById(__name__.substring(1))
+		}else if(__name__.startsWith(".")){
+			this.name = document.getElementsByClassName(__name__.substring(1))
+		}else{
+			this.name = document.getElementsByTagName(__name__)
+		}
+		return this.name
+	}
+}
+
+window.onload = () => {
+	let home = new $("#home")
+	let container = new (".container")
+}
