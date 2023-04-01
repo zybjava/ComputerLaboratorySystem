bb6dd41e4bf4d8edea32cd046708a99826fc7e83
RyannKim327 <rksesgundo123@gmail.com>
2023-04-02 07:19:47 +0800

    04-02-23 07:10

---
+++ backend/add-pc.php
@@ -0,0 +1,24 @@
+<?php
+	include("database.php");
+	if(!isset($_POST['roomname']) || !isset($_POST['com-id']) || !isset($_POST['device-id'])){
+		echo "Error";
+	}else{
+		$sys = new solar_system();
+		$room = $sys->encrypt($_POST['roomname']);
+		$comID = $sys->encrypt($_POST['com-id']);
+		$devID = $sys->encrypt($_POST['device-id']);
+		
+		if(check_pc($conn, $devID)){
+			if(mysqli_query($conn, "INSERT INTO computers (computer_id, device_id, room) VALUES ('$comID', '$devID', '$room')")){
+				echo "Done";
+			}else{
+				echo mysqli_error($conn);
+			}
+		}else{
+			echo "Already Existed";
+		}
+	}
+	function check_pc($conn, $devID) : bool {
+		return mysqli_num_rows(mysqli_query($conn, "SELECT * FROM computers WHERE device_id = '$devID'")) <= 0;
+	}
+?>
--- res/script.js
+++ res/script.js
@@ -7,35 +7,3 @@ let $ = (__name__) => {
 		return document.getElementsByTagName(__name__)
 	}
 }
-
-window.onload = async () => {
-	// Login Registration Panel
-	$("#login").onclick = () => {
-		$("#log-reg-title").textContent = "Login Panel"
-		$("#log-reg-panel").action = "backend/login.php"
-		$("#log-reg-panel").innerHTML = `
-			<span>
-				<label for="username">Student ID: </label>
-					<input type="text" id="username" name="studentID">
-			</span>
-			<span>
-				<label for="password">Password: </label>
-				<input type="password" id="password" name="password">
-			</span>
-			<input type="submit" id="log-reg-submit" value="Login">`
-	}
-	$("#register").onclick = () => {
-		$("#log-reg-title").textContent = "Registration Panel"
-		$("#log-reg-panel").action = "backend/registration.php"
-		$("#log-reg-panel").innerHTML = `
-			<span>
-				<label for="username">Student ID: </label>
-					<input type="text" id="username" name="studentID">
-			</span>
-			<span>
-				<label for="password">Password: </label>
-				<input type="password" id="password" name="password">
-			</span>
-			<input type="submit" id="log-reg-submit" value="Register">`
-	}
-}
