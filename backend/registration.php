808712e26ae3c3522f178656d7568eb06e0c1527
RyannKim327 <rksesgundo123@gmail.com>
2023-04-03 13:38:22 +0800

    04-03-23 13:38

--- backend/registration.php
+++ backend/registration.php
@@ -1,21 +1,28 @@
 <?php
 	include("database.php");
 	$std = new solar_system();
-	if(!isset($_POST['studentID']) || !isset($_POST['password'])){
+	if(!isset($_POST['userID']) || !isset($_POST['password'])){
 		echo "Error";
 	}else{
-		$userID = myData('userID');
-		$firstname = myData('firstname');
-		$middlename = myData('middlename');
-		$lastname = myData('lastname');
-		$office = myData('office');
+		$userID = myData($std, 'userID');
+		$firstname = myData($std, 'firstname');
+		$middlename = myData($std, 'middlename');
+		$lastname = myData($std, 'lastname');
+		$office = myData($std, 'office');
 		$password = sha1($_POST['password']);
 		$password2 = sha1($_POST['password2']);
 
 		if(user($conn, $userID, $password)){
 			if($password == $password2){
-				$sql = mysqli_query($conn, "INSERT INTO users ('userID', 'firstname', 'lastname', 'middlename', 'lastname', 'office', 'password') VALUES ('$userID', '$firstname', '$middlename', '$lastname', '$office', '$password')");	
-			}else{}
+				$sql = mysqli_query($conn, "INSERT INTO users (userID, firstname, middlename, lastname, office, password) VALUES ('$userID', '$firstname', '$middlename', '$lastname', '$office', '$password')");
+				if($sql){
+					echo "{'status': 300, 'redirect': 'add-computer.html'}";
+				}else{
+					echo mysqli_error($conn);
+				}
+			}else{
+				echo "{'status': 400, 'message': 'Password not match'}";
+			}
 		}else{
 			echo "{'b': 'b'}";
 		}
@@ -23,9 +30,7 @@
 	function user($conn, $userID, $password) : bool{
 		return mysqli_num_rows(mysqli_query($conn, "SELECT * FROM users WHERE userID = '$userID' AND password = '$password'")) <= 0;
 	}
-	function myData($key) : string{
-		include("database.php");
-		$std = new solar_system();
+	function myData($std, $key) : string{
 		$result = "";
 		if(isset($_POST[$key])){
 			$result = $std -> encrypt($_POST[$key]);
