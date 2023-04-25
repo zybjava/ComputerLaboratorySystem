1298d8e9802177e9d0f9c20002f02158fefdd0a4
Mr.RySes <mpoprev2@email.com>
2023-04-26 04:36:23 +0800

    04-26-23 04:36 Added Some Buttons with no functions"

--- system/src/pages/ComputerLists.js
+++ system/src/pages/ComputerLists.js
@@ -10,11 +10,27 @@ export default function ComputerLists(){
 		})
 	})
 
+	const UpdateDesign = {
+		backgroundColor: "#131320aa",
+		color: "#00ff00",
+		padding: "0.25em 0.75em",
+		borderRadius: "1em",
+		border: "1px #00ff00 solid"
+	}
+
+	const DeleteDesign = {
+		backgroundColor: "#131320aa",
+		color: "#ff0000",
+		padding: "0.25em 0.75em",
+		borderRadius: "1em",
+		border: "1px #ff0000 solid"
+	}
+
 	return (
 		<div className='App-main'>
 			{data.map(r => {
 				return (
-					<table>
+					<table className='Class-table'>
 						<tbody>
 							<tr>
 								<th>Computer ID</th>
@@ -22,6 +38,8 @@ export default function ComputerLists(){
 								<th>Monitor ID</th>
 								<th>Room Name</th>
 								<th>Department ID</th>
+								<th></th>
+								<th></th>
 							</tr>
 							<tr>
 								<td>{r.computerID}</td>
@@ -29,6 +47,12 @@ export default function ComputerLists(){
 								<td>{r.monitorID}</td>
 								<td>{r.roomName}</td>
 								<td>{r.departmentID}</td>
+								<td>
+									<button style={UpdateDesign}>Update</button>
+								</td>
+								<td>
+									<button style={DeleteDesign}>Delete</button>
+								</td>
 							</tr>
 						</tbody>
 					</table>
